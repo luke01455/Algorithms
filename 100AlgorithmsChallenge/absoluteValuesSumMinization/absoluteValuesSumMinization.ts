3bbae7249edbcb1699cd4f4514f93e4d1ea0217e
luke01455 <luke0145555@hotmail.co.uk>
2019-09-04 20:44:48 +0100

    solved median and addition algorithms

--- 100AlgorithmsChallenge/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ 100AlgorithmsChallenge/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,13 @@
 function absoluteValuesSumMinimization(a: number[]): number {
 
+const ans = a[Math.floor((a.length + 1) / 2) -1]
+
+return ans;
+
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+ console.log(absoluteValuesSumMinimization([2, 4, 7]));
+ console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8, 12, 18, 22, 44, 84, 99]))
