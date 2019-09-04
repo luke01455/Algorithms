d012d1fc1798f70553ec1b524631833e76babf81
luke01455 <luke0145555@hotmail.co.uk>
2019-09-04 21:40:11 +0100

    solved asterisk border algorithm

--- 100AlgorithmsChallenge/addBorder/addBorder.ts
+++ 100AlgorithmsChallenge/addBorder/addBorder.ts
@@ -1,5 +1,28 @@
 function addBorder(picture: string[]): string[] {
+const newArr = [];
 
+     const addAsterisk = () => {
+        const asterisks = [];
+        for(let i = 0; i < picture[0].length + 2; i++) {
+            asterisks.push('*');
+        }
+        return asterisks.join("");
+    }
+
+    const asts = addAsterisk();
+
+    newArr.unshift(asts);
+    for(let i = 0; i < picture.length; i++) {
+        newArr.push('*'.concat(picture[i], '*'));
+    }
+    newArr.push(asts);
+
+    return newArr;
+
+// const lengthOfWall = picture[0].length + 2;
+
+// for(let i = 0; i < lengthOf)
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["httgfjuiopsd", "fgnfghguiopo"]));
