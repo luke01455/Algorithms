cec27f2beddc7178a468fffebaf6f7037c9b6687
luke01455 <luke0145555@hotmail.co.uk>
2019-09-05 21:30:53 +0100

    solved algo to find and print the longest strings in an array

--- 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
+++ 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 function allLongestStrings(inputArray: string[]): string[] {
 
+    let longestLength = 0;
+
+    inputArray.forEach((word: string) => {
+        longestLength = longestLength < word.length ? word.length : longestLength;
+    });
+
+    const newArr = [];
+    inputArray.forEach((word: string) => {
+        if(word.length === longestLength) {
+            newArr.push(word);
+        }
+    })
+
+    return newArr;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
