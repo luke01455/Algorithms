198bd551c49b3d99534a21ab3269635aab9a8cfb
luke01455 <luke0145555@hotmail.co.uk>
2019-09-05 17:55:09 +0100

    adding digits algo solved

--- 100AlgorithmsChallenge/addTwoDigits/addTwoDigits.ts
+++ 100AlgorithmsChallenge/addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,15 @@
 function addTwoDigits(n: any): number {
+    const numArr = n.toString().split('');
+
+
+    
+
+    const addFunc = (total, num) => {
+        return parseInt(total) + parseInt(num);
+    }
+
+    return numArr.reduce(addFunc);
 
 }
 
-// console.log(addTwoDigits(29));
+ console.log(addTwoDigits(29));
