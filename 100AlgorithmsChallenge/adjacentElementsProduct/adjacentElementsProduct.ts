f8b98601a8e4eeea0874b14ef2250b8dff0fd155
luke01455 <luke0145555@hotmail.co.uk>
2019-09-05 19:36:08 +0100

    completed adding adjacent elements algo

--- 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,20 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray: number[]): any {
+
+    const resultsArr = [];
+    for(let i = 0; i < inputArray.length; i++) {
+        resultsArr.push(inputArray[i] * inputArray[i + 1])
+    }
+
+    let highestNum = 0;
+    for(let i = 0; i < inputArray.length - 1; i++) {
+        if(resultsArr[i] >= highestNum) {
+            highestNum = resultsArr[i];
+        }
+    }
+
+
+    return highestNum;
 
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
