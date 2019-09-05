2e94148e0f418a8d7329b6440a9747a19fa31253
luke01455 <luke0145555@hotmail.co.uk>
2019-09-05 22:05:38 +0100

    solved algo almostincreasingsequence

--- 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
+++ 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,21 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
 
+    let count = 0;
+    for(let i = 0; i < sequence.length - 1; i++) {
+        if(sequence[i] < sequence[i + 1]) {
+           continue;
+         } else {
+             count += 1;
+         }
+    }
+
+    const result = count < 2 ? true : false;
+
+    return result;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+ console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+ console.log(almostIncreasingSequence([1, 3, 2])) 
+ console.log(almostIncreasingSequence([1, 3, 2, 6, 7]))
+ console.log(almostIncreasingSequence([1, 3, 2, 2, 7]))
+ console.log(almostIncreasingSequence([1, 3, 2, 5, 7, 10, 11]))
