6fc99641a78f9a8ce2d8194a4e59fa572a00c815
luke01455 <luke0145555@hotmail.co.uk>
2019-09-06 16:43:29 +0100

    alphabetical shifting algo solved, better methods would be key and ascii

--- 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
+++ 100AlgorithmsChallenge/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -14,8 +14,11 @@ function almostIncreasingSequence(sequence: number[]): boolean {
     return result;
 }
 
- console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+ console.log(almostIncreasingSequence([1, 3, 1, 1])) 
  console.log(almostIncreasingSequence([1, 3, 2])) 
  console.log(almostIncreasingSequence([1, 3, 2, 6, 7]))
  console.log(almostIncreasingSequence([1, 3, 2, 2, 7]))
  console.log(almostIncreasingSequence([1, 3, 2, 5, 7, 10, 11]))
+ console.log(almostIncreasingSequence([10, 1, 2, 3, 4, 5]));
+console.log(almostIncreasingSequence([1, 2, 3, 4, 99, 5, 6]));
+console.log(almostIncreasingSequence([1, 2, 2, 3])); 
--- 100AlgorithmsChallenge/alphabeticShift/alphabeticShift.ts
+++ 100AlgorithmsChallenge/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,24 @@
 function alphabeticShift(inputString: string): string {
+   const alphabet: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+   const origArr = inputString.split("");
+    const newArr = [];
+   origArr.forEach((letter) => {
+       for(let i = 0; i < alphabet.length; i++) {
+           if (letter === alphabet[i] && letter !== 'z') {
+            newArr.push(alphabet[i+1]);
+            break;
+           }
+           else if (letter === alphabet[alphabet.length - 1]){
+               newArr.push('a');
+               break;
+           }
+       }
+   })
+
+   const ans = newArr.join();
+
+   return ans;
 }
 
 console.log(alphabeticShift('crazy'));
