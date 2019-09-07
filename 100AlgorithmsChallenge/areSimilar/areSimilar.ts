9b43bb56352554ade68f6b7cea5c35147a84113b
luke01455 <luke0145555@hotmail.co.uk>
2019-09-07 17:17:42 +0100

    try this again later, areSimilar arr

--- 100AlgorithmsChallenge/areSimilar/areSimilar.ts
+++ 100AlgorithmsChallenge/areSimilar/areSimilar.ts
@@ -1,5 +1,30 @@
-function areSimilar(a: number[], b: number[]): boolean {
+function areSimilar(a: number[], b: number[]): any {
    
+    const aArr = a.toString();
+    const bArr = b.toString();
+    if (aArr === bArr) {
+        return true;
+    }
+
+    const tempArr = [];
+    for(let i = 0; i < b.length -1; i++)
+    {
+        const replace = a.slice(0);
+        replace[i] = replace[i+1];
+        replace[i+1] = replace[i];
+        const aArr2 = a.toString();
+        const bArr2 = b.toString();
+
+        if (aArr2 === bArr2) {
+            return true;
+ 
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
