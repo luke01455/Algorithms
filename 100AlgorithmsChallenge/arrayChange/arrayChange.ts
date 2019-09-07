47b039b28a97c158253f16bbab47ff5133f94807
luke01455 <luke0145555@hotmail.co.uk>
2019-09-07 19:12:27 +0100

    solved array change counting algo

--- 100AlgorithmsChallenge/arrayChange/arrayChange.ts
+++ 100AlgorithmsChallenge/arrayChange/arrayChange.ts
@@ -1,5 +1,49 @@
-function arrayChange(inputArray: number[]): number {
+// function arrayChange(inputArray: number[]): number {
  
-}
+//     const inputArra = inputArray;
+//     let count = 0;
+//     for(let i = 1; i < inputArra.length; i++ ) {
+//         if(inputArra[i] < inputArra[i-1]) {
+//             while(inputArra[i] < inputArra[i-1]) {
+//                 inputArra[i]++;
+//                 count++;
+//             }
+//             inputArra[i] ++;
+//             count += 1;
 
+            
+//     } else if (inputArra[i] > inputArra[i-1]) {
+        
+//         while(inputArra[i] > inputArra[i-1]) {
+//             inputArra[i]--;
+//             count++;
+//         }
+//         console.log(inputArra[i])
+//         inputArra[i] += 1;
+//         count = count - 1;
+//     } else {
+//         inputArra[i] += 1;
+//         count += 1;
+//     }
+
+
+// }
+// return count;
+// }
+function arrayChange(arr: number[]): number {
+ 
+    let turnsNeeded: number = 0;
+ 
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] <= arr[i - 1]){
+            turnsNeeded += (arr[i -1] + 1) - arr[i];
+            arr[i] = arr[i - 1] + 1;
+        }
+    }
+    return turnsNeeded;
+}
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 4, 3]));
+console.log(arrayChange([5, 4, 3]));
+console.log(arrayChange([10, 4, 15]));
+console.log(arrayChange([9,1,1]));
