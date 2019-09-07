a2efb7952ed5cd5fbea8293b2cd997a521818e64
luke01455 <luke0145555@hotmail.co.uk>
2019-09-07 15:43:27 +0100

    equally strong algo, would have been much easier to use addition and subtraction also should have used a function in my method

--- 100AlgorithmsChallenge/areEquallyStrong/areEquallyStrong.ts
+++ 100AlgorithmsChallenge/areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,32 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+let mStrong = 0;
+let fStrong = 0;
+let mWeak = 0;
+let fWeak= 0;
 
+if (yourRight >= yourLeft) {
+    mStrong = yourRight;
+    mWeak = yourLeft;
+} else {
+    mStrong = yourLeft;
+    mWeak = yourRight;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+if (friendsRight >= friendsLeft) {
+    fStrong = friendsRight;
+    fWeak = friendsLeft;
+} else {
+    fStrong = friendsLeft;
+    fWeak = friendsRight;
+}
+
+if (mStrong === fStrong && mWeak === fWeak) {
+    return true;
+} else {
+    return false;
+}
+}
+
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
