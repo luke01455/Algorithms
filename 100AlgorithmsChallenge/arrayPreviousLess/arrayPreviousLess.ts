e426f0ec3cd8e65341b4644c3e7b4a39ce7abbb6
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 15:16:26 +0100

    managed to hack this one a bit, not too sure how i did it

--- 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
+++ 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,25 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+ const newArr = [];
+    for(let i = 0; i < items.length; i++) {
+        for(let j = i; j >= 0; j--) {
+            if(items[i] > items[j]) {
+                newArr.push(items[j])
+                break;
+            }
+            if(!newArr[i]) {
+                newArr.push(-1)
+            }
+        }
+    }
+    newArr.shift();
+    return newArr;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 5, 4, 5, 9]));
+console.log(arrayPreviousLess([11, 3, 3, 12, 5, 5]));
+console.log(arrayPreviousLess([66, 22, 3, 44, 33, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([0, 9, 4, 11, 9]));
+console.log(arrayPreviousLess([52, 95, 21, 3, 55]));
+console.log(arrayPreviousLess([69, 1, 200, 350, 60]));
