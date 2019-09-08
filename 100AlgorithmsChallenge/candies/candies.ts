ddef74419f30919771588d0afa0abb3407f12f74
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 18:30:16 +0100

    daft canides algo done

--- 100AlgorithmsChallenge/candies/candies.ts
+++ 100AlgorithmsChallenge/candies/candies.ts
@@ -1,5 +1,9 @@
 function candies(n: number, m: number): number {
 
+    let ans = m / n;
+    return Math.floor(ans) * n;
 }
 
 console.log(candies(3, 10));
+console.log(candies(3, 55));
+console.log(candies(3, 9));
