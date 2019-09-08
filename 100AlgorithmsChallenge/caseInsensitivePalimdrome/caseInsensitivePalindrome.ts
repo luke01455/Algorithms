459d80cc125929d0efdf827865fcded297fdaa5a
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 19:04:04 +0100

    case insensitive palindrone algo solved

--- 100AlgorithmsChallenge/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ 100AlgorithmsChallenge/caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,13 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    const newStr= [];
+    
+    const splitStr = inputString.split('')
+    console.log(splitStr);
+    for (let i = splitStr.length -1 ; i >= 0; i--) {
+        newStr.push(splitStr[i]);
+    }
+    
+    return newStr.join("").toLowerCase() === splitStr.join("").toLowerCase()
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
