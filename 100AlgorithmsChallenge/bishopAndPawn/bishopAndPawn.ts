30ed080ea8dd2840804276233bc3c99e0f435897
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 17:39:59 +0100

    bishop and pawn algo solved, very happy with this one

--- 100AlgorithmsChallenge/bishopAndPawn/bishopAndPawn.ts
+++ 100AlgorithmsChallenge/bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,22 @@
-function bishopAndPawn(bishop: string, pawn: string): boolean {
+function bishopAndPawn(bishop, pawn): boolean {
+const bishopLoc = bishop.split('');
+const pawnLoc = pawn.split('');
 
+bishopLoc[0] = bishopLoc[0].charCodeAt(0);
+pawnLoc[0] = pawnLoc[0].charCodeAt(0);
+
+var difference = Math.abs(bishopLoc[0] - pawnLoc[0]);
+
+if (difference === Math.abs(bishopLoc[1] - pawnLoc[1])) {
+    return true;
+} else {
+    return false;
+}
 }
 
 console.log(bishopAndPawn('a1', 'c3'));
+
+console.log(bishopAndPawn('B2', 'D4'));
+console.log(bishopAndPawn('D6', 'G3'));
+console.log(bishopAndPawn('F5', 'C8')); 
+console.log(bishopAndPawn('H6', 'H7'));
