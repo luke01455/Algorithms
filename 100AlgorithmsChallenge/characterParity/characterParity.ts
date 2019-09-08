323dfaaeace87cf1dd2701648884c34e086493f0
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 19:42:16 +0100

    simple char parity algo solved

--- 100AlgorithmsChallenge/characterParity/characterParity.ts
+++ 100AlgorithmsChallenge/characterParity/characterParity.ts
@@ -1,5 +1,16 @@
 function characterParity(symbol: string): string {
    
+   const result = parseInt(symbol);
+
+    if (result % 2 === 0) {
+        return 'even';
+    }
+    else if (isNaN(result)) {
+        return 'is not a number';
+    }
+    else {
+        return 'odd';
+    }
 }
 
 console.log(characterParity('5'))
