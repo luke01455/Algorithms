120ab9498d06337bfd59045c5ed778dcc0ea3ebc
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 12:49:50 +0100

    maximum adjacent diff algo solved

--- 100AlgorithmsChallenge/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ 100AlgorithmsChallenge/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,11 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
 
+    let max = 0;
+    for(let i = 0; i < inputArray.length -1; i++) {
+        max = Math.abs(inputArray[i] - inputArray[i+1]) > max ? Math.abs(inputArray[i] - inputArray[i+1]) : max
+    }
+return max;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([1, 22, 88, 55]));
