315e9730ad70899e0e5c13cd9e9d9ea0887b1d7f
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 19:29:19 +0100

    solved century return algo but commented mine because ceil was a lot smarter

--- 100AlgorithmsChallenge/centuryFromYear/centuryFromYear.ts
+++ 100AlgorithmsChallenge/centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,14 @@
 function centuryFromYear(year: number): number {
  
+// return Math.floor((year - 1) / 100) + 1 ;
+return Math.ceil(year / 100);
 }
 
 console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1650));
 console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1701));
+console.log(centuryFromYear(1702));
+console.log(centuryFromYear(1799));
+console.log(centuryFromYear(1800));
+console.log(centuryFromYear(1801));
