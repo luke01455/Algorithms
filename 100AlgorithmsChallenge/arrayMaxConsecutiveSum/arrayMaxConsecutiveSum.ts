79af295efb340606f8df3c8d04d731d0b082aa84
luke01455 <luke0145555@hotmail.co.uk>
2019-09-08 12:35:16 +0100

    look at this again, confusing

--- 100AlgorithmsChallenge/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ 100AlgorithmsChallenge/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,26 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
 
+    let sum = 0;
+    let max = 0;
+
+    for (let i = 0; i < k; i++){
+        sum += inputArray[i]
+        console.log(sum)
+    }
+
+    for (let i = k; i < inputArray.length; i++) {
+        sum -= inputArray[i-k];
+        sum += inputArray[i];
+
+        if(sum > max) {
+            max = sum;
+        }
+        console.log(sum)
+    }
+    console.log(max)
+    return max;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+
+//console.log(arrayMaxConsecutiveSum([2, 3, 10, 1, 6], 3));
