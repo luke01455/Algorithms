baea34a40d231b08bb70890eed56587e575707bd
luke01455 <luke0145555@hotmail.co.uk>
2019-09-09 11:32:22 +0100

    added someone elses method as its 10x better and smarter

--- 100AlgorithmsChallenge/chunkyMonkey/chunkyMonkey.ts
+++ 100AlgorithmsChallenge/chunkyMonkey/chunkyMonkey.ts
@@ -1,29 +1,14 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
 
-    let currArr = [];
-
-    while(currArr.length < arr.length) {
-    for(let i = currArr.length; i <= arr.length ; i += size) {
-        currArr.push(arr.slice(i, i + size));  
+    let bigArray = [];
+ 
+    while (arr.length > 0) {
+        bigArray.push(arr.splice(0, size));
     }
-
-    for(let i = 0; i < currArr.length; i++) {
-            var filtered = currArr.filter(function(value, index, arr){
-
-                return index < size;
-            
-            });
-        
-    }
-    
-    return filtered;
+ 
+    return bigArray;
 }
-console.log(filtered)
-console.log(currArr.length)
-console.log(arr.length)
-    console.log(arr);
-    console.log(currArr);
-}
+
 
 
 
@@ -32,4 +17,5 @@ console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
 console.log(chunkyMonkey([0, 1, 2, 4, 4, 6], 7));
 console.log(chunkyMonkey([0, 1, 3, 3, 4, 5], 5));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 2));
 
