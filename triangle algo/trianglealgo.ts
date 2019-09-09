50a6ec5113e5e57db94cfad47cf3e134d6277ebc
luke01455 <luke0145555@hotmail.co.uk>
2019-09-09 15:10:48 +0100

    triangle algo from codewars done

---
+++ triangle algo/trianglealgo.ts
@@ -0,0 +1,10 @@
+function isTriangle(a,b,c)
+{
+    if (a + b > c && a + c > b && c + b > a) {
+        return true;
+    }
+        return false;  
+}
+
+console.log(isTriangle(1,2,2));
+console.log(isTriangle(7,2,2));
