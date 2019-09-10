3746031fca1e20dd5fc8f618515102df10c42ccb
luke01455 <luke0145555@hotmail.co.uk>
2019-09-10 21:09:08 +0100

    easy confirm ending algo solved

--- 100AlgorithmsChallenge/confirmEnding/confirmEnding.ts
+++ 100AlgorithmsChallenge/confirmEnding/confirmEnding.ts
@@ -1,6 +1,13 @@
 function confirmEnding(str: string, target: string) {
-   
+   let strLength = str.length;
+   let tarLength = target.length;
+
+   if(str.substring(strLength-tarLength) === target) {
+       return true;
+   }
+   else return false;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("god is my owner", "ner"));
