cef0b840c93168f844e3eb553adb77d9e6e9ba82
luke01455 <luke0145555@hotmail.co.uk>
2019-09-11 20:34:42 +0100

    very similar to last one, another easy one

--- 100AlgorithmsChallenge/containsDuplicates/containsDuplicates.ts
+++ 100AlgorithmsChallenge/containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,15 @@
 function containsDuplicates(a: number[]): boolean {
+    for(let i = 0; i < a.length; i++) {
+        for(let j = 0; j < a.length; j++) {
+            if(i !== j) {
+                if(a[i] === a[j]) {
+                    return true;
+                }
+            }
+        }
+    }
 
+    return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
