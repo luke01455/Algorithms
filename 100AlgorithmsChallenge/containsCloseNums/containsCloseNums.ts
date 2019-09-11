061769023181c9080590428e3f34e6e137e803b7
luke01455 <luke0145555@hotmail.co.uk>
2019-09-11 19:38:05 +0100

    quick algo solved

--- 100AlgorithmsChallenge/containsCloseNums/containsCloseNums.ts
+++ 100AlgorithmsChallenge/containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,22 @@
 function containsCloseNums(nums: number[], k: number): boolean {
+    const repeatsArray = [];
 
-}
+        for (let i  = 0; i < nums.length; i++)  {
+            for(let j = nums[i] + 1; j < nums.length; j++) {
+                if(nums[i] === nums[j]) {  
+                    repeatsArray.push(i)
+                }
+            }
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            
+        }
+
+    console.log(repeatsArray)
+
+    return (Math.abs(repeatsArray[0] - repeatsArray[1]) <= k) ? true : false;
+ }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
