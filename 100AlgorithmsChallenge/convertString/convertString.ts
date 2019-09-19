3a6642010f8252c02e3ebc7f2c1a2e45804d9803
luke01455 <luke0145555@hotmail.co.uk>
2019-09-19 20:26:53 +0100

    crossingSum algo

--- 100AlgorithmsChallenge/convertString/convertString.ts
+++ 100AlgorithmsChallenge/convertString/convertString.ts
@@ -1,6 +1,25 @@
 function convertString(s: string, t: string): boolean {
+const trueArray = [];
+console.log(s)
+    for(let i = 0; i < s.length; i++) {
+        for(let j = 0; j < s.length; j++) {
+                console.log(s[j])
+                console.log(t[j])
+                if(t[i] === s[j]) {
+                    
+                    trueArray.push(t[i]);
+                    break;
+                }
+        }
 
+    }
+    const newArr = trueArray.join("")
+    console.log(newArr)
+    console.log(trueArray)
+    return trueArray.join("") === t ? true : false;
+   
+    console.log(trueArray)
 }
 
-console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
+//console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
 console.log(convertString('addbyca', 'abcd'));
