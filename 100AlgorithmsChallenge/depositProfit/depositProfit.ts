b0c73583e4eaf5ca68956b350c0b139d7029aaf2
luke01455 <luke0145555@hotmail.co.uk>
2019-09-19 21:13:56 +0100

    deposit profit algorithm solved

--- 100AlgorithmsChallenge/depositProfit/depositProfit.ts
+++ 100AlgorithmsChallenge/depositProfit/depositProfit.ts
@@ -1,5 +1,15 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+ const percentage = (rate / deposit) + 1
+
+ let count = 0;
+
+ for(let i = deposit; i <= threshold; i = i*=percentage) {
+    count++
+    console.log(i)
+ }
+
+ return count;
 }
 
-console.log(depositProfit(100, 20, 170))
+//console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 35, 190))
