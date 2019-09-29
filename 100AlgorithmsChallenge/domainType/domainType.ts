ca9fb5ead4c2e23a0916679567ad5cd5acfeb81d
luke01455 <luke0145555@hotmail.co.uk>
2019-09-29 23:30:16 +0100

    domain types algo solved

--- 100AlgorithmsChallenge/domainType/domainType.ts
+++ 100AlgorithmsChallenge/domainType/domainType.ts
@@ -1,5 +1,23 @@
 function domainType(domains: string[]): string[] {
+const returnArr = []
+    for(let i = 0; i < domains.length; i++) {
+        if(domains[i].includes(".com")) {
+            returnArr.push("commercial");
+        }
+        else if (domains[i].includes(".org")) {
+            returnArr.push("orgnisation")
+        }
+        else if(domains[i].includes(".net")) {
+            returnArr.push("network")
+        }
+        else if(domains[i].includes(".info")) {
+            returnArr.push("information")
+        } else {
+            returnArr.push("unknown")
+        }
+    }
 
+    return returnArr
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
