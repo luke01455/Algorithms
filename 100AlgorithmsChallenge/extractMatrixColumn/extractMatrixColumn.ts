a3a7a98ca814b287aa2af9dc7c6935c66c65345a
luke01455 <luke0145555@hotmail.co.uk>
2019-11-15 18:59:06 +0000

    extract matrix columns algorithm solved

--- 100AlgorithmsChallenge/extractMatrixColumn/extractMatrixColumn.ts
+++ 100AlgorithmsChallenge/extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,10 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+let newArray = [];
 
+for(let i = 0; i < matrix.length; i++) {
+    newArray.push(matrix[i][column])
+}
+return newArray;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
