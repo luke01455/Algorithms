39c98d61b0a011a670ab224ed18110b0ca0ff45c
luke01455 <luke0145555@hotmail.co.uk>
2020-01-06 14:02:31 +0000

    uber fares algorithm

--- 100AlgorithmsChallenge/fancyRide/fancyRide.ts
+++ 100AlgorithmsChallenge/fancyRide/fancyRide.ts
@@ -1,5 +1,13 @@
-function fancyRide(l: number, fares: number[]): string {
+function fancyRide(l, fares) {
+    let winner = 0
 
+    for(let i = 0; i < fares.length; i++){
+        if(fares[i] * 20 < l && fares[i] >= winner) {
+            winner = fares[i]
+        }
+    }
+
+    return winner
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
