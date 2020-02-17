078d2bdd12c96bf2acc428b958acae1f8ed17c38
luke01455 <luke0145555@hotmail.co.uk>
2020-02-17 18:50:43 +0000

    pretty easy uber algorithm solved

--- 100AlgorithmsChallenge/fareEstimator/fareEstimator.ts
+++ 100AlgorithmsChallenge/fareEstimator/fareEstimator.ts
@@ -1,5 +1,11 @@
 function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
+    const finalPrices = []
 
+    for(let i = 0; i < cost_per_minute.length; i++) {
+        finalPrices.push(cost_per_minute[i] * ride_time + cost_per_mile[i] * ride_distance)
+    }
+
+    return finalPrices
 }
 
 console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
